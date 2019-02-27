8411b3acec149e839bf887802910a5dae2397933
Thomas Liu <liuxianke@letv.com>
2019-02-27 14:16:19 +0800

    <CM>: initialize project files.

---
+++ Android.mk
@@ -0,0 +1,22 @@
+# Copyright 2005 The Android Open Source Project
+#
+# Android.mk for adb
+#
+
+LOCAL_PATH := $(call my-dir)
+
+LOCAL_SRC_FILES := \
+	hello.c
+
+LOCAL_CFLAGS := \
+	-O2 \
+	-g \
+	-Wall \
+	-Wno-unused-parameter
+
+LOCAL_SHARED_LIBRARIES := \
+	libc
+
+LOCAL_MODULE := hello
+
+include $(BUILD_EXECUTABLE)
